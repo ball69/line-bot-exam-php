7baba988f945470a16a269dfd0f9d741ce76349f
BALLYEAH-PC-6LD\ballyeah <skaball69@gmail.com>
2018-05-10 23:18:00 +0700

    ddd

--- index.php
+++ index.php
@@ -1,3 +1,2 @@
 <?php
-echo "test";
 echo "Hello LINE BOT";
--- profile.php
+++ profile.php
@@ -1,9 +1,9 @@
 <?php
 
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'F91qn9+cosBSeiXmO5rTLn1pAU+yUw1AxXvGDPRtlL4IO8GgIwwdXIVb/HgPkoAcQh1InmIbH1kOl8OI5DkoM3vw3gejfxXL5iyn5OJ+dQTQ6VRd7QMk555tNZQOY7uQf6B5sH6tBusMXtpaeRyFaAdB04t89/1O/w1cDnyilFU=';
 
-$userId = 'Uffa138efe037e6e889d0b0f4a871c005';
+$userId = 'U3751e9be717b7e8cc5b28e6fc22066aa';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
