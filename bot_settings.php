98b34b855bb5f1132a1a531ea0d8db6cbd9d7b98
BALLYEAH-PC-6LD\ballyeah <skaball69@gmail.com>
2018-05-11 00:21:14 +0700

    dd

---
+++ bot_settings.php
@@ -0,0 +1,6 @@
+<?php
+/// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
+define('LINE_MESSAGE_CHANNEL_ID','158033001102');
+define('LINE_MESSAGE_CHANNEL_SECRET','1b33b8ecf28ddb587f0b7e15875ef701');
+define('LINE_MESSAGE_ACCESS_TOKEN','F91qn9+cosBSeiXmO5rTLn1pAU+yUw1AxXvGDPRtlL4IO8GgIwwdXIVb/HgPkoAcQh1InmIbH1kOl8OI5DkoM3vw3gejfxXL5iyn5OJ+dQTQ6VRd7QMk555tNZQOY7uQf6B5sH6tBusMXtpaeRyFaAdB04t89/1O/w1cDnyilFU=');
+?>
