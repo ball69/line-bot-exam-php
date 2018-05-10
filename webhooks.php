e5825b3108bf835960ac760281710da04170dd8f
BALLYEAH-PC-6LD\ballyeah <skaball69@gmail.com>
2018-05-11 00:01:40 +0700

    asdasd

--- webhooks.php
+++ webhooks.php
@@ -14,7 +14,7 @@ if (!is_null($events['events'])) {
 // Reply only when message sent is in 'text' format
         if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 // Get text sent
-            $text = $event['source']['userId'];
+            $text = 'yeah yeah yeah';
 // Get replyToken
             $replyToken = $event['replyToken'];
 // Build message to reply back
@@ -42,4 +42,3 @@ if (!is_null($events['events'])) {
         }
     }
 }
-echo "OK";
