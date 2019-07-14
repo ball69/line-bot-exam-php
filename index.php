52f2e72e5ab6f199f5e1851d653d6ac17152e32c
ballyeah <skaball69@gmail.com>
2019-07-14 21:44:30 +0700

    change token

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 require 'vendor/autoload.php';
 
-$token = 'YOUR LINE NOTIFY TOKEN';
+$token = 'GfU8F9KoNTxMIMa7oZ2ZR/MLdWQ6JqmgW+JC+18STtapNz3hlczHz7MFH65y/cigdqm+lYHAR6LBsUeyHgkhnuaCdc/iSeMAz8DMmh7YG8QABiVBidCEd1tqdyp5Y7xG2SmUjgNxD1ZXmicmJKb06AdB04t89/1O/w1cDnyilFU=';
 $ln = new KS\Line\LineNotify('7nVEjAM8Cv5nIJWGwGYFYDhffReNi3rg0CsnuBinGiP');
 
 $text = 'Hello Line Notify';
